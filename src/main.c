c2065e406cf3bcf1c2145082a76596c757173f3c
Levi Rak <ldr423@gmail.com>
2021-06-18 08:18:24 -0600

    ingest the spreadsheet into memory

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 
 #include <ctype.h>
 #include <errno.h>
+#include <limits.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -135,7 +136,7 @@ struct row_lexer {
 };
 
 static enum token_type
-NextRowToken(struct row_lexer *State, char *Buf, umm Sz)
+NextCell(struct row_lexer *State, char *Buf, umm Sz)
 {
     Assert(State);
     Assert(Buf);
@@ -198,68 +199,190 @@ NextCmdToken(struct cmd_lexer *State, char *Buf, umm Sz)
 }
 
 
+struct cell {
+    enum token_type Type;
+    char Str[32]; /* TODO(lrak): dynamic */
+};
+
+struct document {
+    s32 NumCol, MaxCol;
+    s32 NumRow, MaxRow;
+    struct cell *Cells;
+
+    s32 FirstBodyRow;
+    s32 FirstFootRow;
+};
+
+static struct cell *
+GetCell(struct document *Doc, s32 Col, s32 Row)
+{
+    Assert(Doc);
+    Assert(Col >= 0);
+    Assert(Row >= 0);
+
+    if (Col >= Doc->MaxCol || Row >= Doc->MaxRow) {
+        /* resize */
+        smm NewMaxCol = MAX(Doc->MaxCol, 8);
+        smm NewMaxRow = MAX(Doc->MaxRow, 8);
+        while (Col > NewMaxCol) NewMaxCol *= 2;
+        while (Row > NewMaxRow) NewMaxRow *= 2;
+
+        umm NewSize = sizeof *Doc->Cells * NewMaxCol * NewMaxRow;
+        struct cell *NewCells = malloc(NewSize);
+        memset(NewCells, 0, NewSize);
+
+        if (Doc->Cells) {
+            for (s32 ColIdx = 0; ColIdx < Doc->NumCol; ++ColIdx) {
+                for (s32 RowIdx = 0; RowIdx < Doc->NumRow; ++RowIdx) {
+                    s32 Idx = Col + Row*Doc->MaxCol;
+                    NewCells[Idx] = Doc->Cells[Idx];
+                }
+            }
+        }
+
+        Doc->Cells = NewCells;
+        Doc->MaxCol = NewMaxCol;
+        Doc->MaxRow = NewMaxRow;
+    }
+
+    Doc->NumCol = MAX(Doc->NumCol, Col+1);
+    Doc->NumRow = MAX(Doc->NumRow, Row+1);
+    Assert(Doc->NumCol <= Doc->MaxCol);
+    Assert(Doc->NumRow <= Doc->MaxRow);
+
+    Assert(Doc->Cells);
+    return Doc->Cells + (Col + Row*Doc->MaxCol);
+}
+
+static void
+PrintDocument(struct document *Doc)
+{
+    Assert(Doc);
+    for (s32 Row = 0; Row < Doc->NumRow; ++Row) {
+        if (Row == Doc->FirstBodyRow) printf(" ↑head body↓\n");
+        if (Row == Doc->FirstFootRow) printf(" ↑body foot↓\n");
+        for (s32 Col = 0; Col < Doc->NumCol; ++Col) {
+            struct cell *Cell = Doc->Cells + (Col + Row*Doc->MaxCol);
+            printf("[%.*s]", (s32)sizeof Cell->Str, Cell->Str);
+        }
+        printf("\n");
+    }
+}
+
+static struct document *
+MakeDocument(char *Path)
+{
+    Assert(Path);
+
+    FILE *File;
+    struct document *Doc = 0;
+
+    if (!(File = fopen(Path, "r"))) {
+        LogError("fopen");
+    }
+    else {
+        NotNull(Doc = malloc(sizeof *Doc));
+        *Doc = (struct document){
+            .FirstBodyRow = 0,
+            .FirstFootRow = INT32_MAX,
+        };
+
+        char Line[1024];
+        enum line_type LineType;
+
+        umm RowIdx = 0;
+        while ((LineType = ReadLine(File, Line, sizeof Line))) {
+            char *Prefix = "UNK";
+            switch (LineType) {
+            case LINE_EMPTY:   Prefix = "NUL"; break;
+            case LINE_ROW:     Prefix = "ROW"; break;
+            case LINE_COMMAND: Prefix = "COM"; break;
+            case LINE_COMMENT: Prefix = "REM"; break;
+            InvalidDefaultCase;
+            }
+
+            printf("%s.", Prefix);
+            switch (LineType) {
+            case LINE_EMPTY:
+                if (Doc->FirstBodyRow == 0) {
+                    Doc->FirstBodyRow = RowIdx;
+                }
+                else {
+                    Doc->FirstFootRow = RowIdx;
+                }
+                break;
+
+            case LINE_ROW: {
+                char Buf[512];
+                enum token_type Type;
+                struct row_lexer Lexer = { Line };
+
+                umm ColIdx = 0;
+                while ((Type = NextCell(&Lexer, Buf, sizeof Buf))) {
+                    switch (Type) {
+                    case TOKEN_STATIC_CELL: printf("[%s]", Buf); break;
+                    case TOKEN_EXPR_CELL: printf("{%s}", Buf); break;
+                    InvalidDefaultCase;
+                    }
+
+                    struct cell *Cell;
+                    NotNull(Cell = GetCell(Doc, ColIdx, RowIdx));
+                    Cell->Type = Type;
+                    strncpy(Cell->Str, Buf, sizeof Cell->Str);
+                    ++ColIdx;
+                }
+                ++RowIdx;
+            } break;
+
+            case LINE_COMMAND: {
+                char Buf[512];
+                enum token_type Type;
+                struct cmd_lexer Lexer = { Line };
+
+                while ((Type = NextCmdToken(&Lexer, Buf, sizeof Buf))) {
+                    switch (Type) {
+                    case TOKEN_IDENT:
+                        printf("(%s)", Buf);
+                        break;
+                    InvalidDefaultCase;
+                    }
+                }
+            } break;
+
+            case LINE_COMMENT: printf("%s", Line); break;
+            default: break;
+            }
+            printf("\n");
+        }
+
+        fclose(File);
+    }
+
+    return Doc;
+}
+
+static void
+DeleteDocument(struct document *Doc)
+{
+    if (Doc) {
+        free(Doc->Cells);
+        free(Doc);
+    }
+}
+
+
 s32
 main(s32 ArgCount, char **Args)
 {
     for (s32 Idx = 1; Idx < ArgCount; ++Idx) {
-        FILE *File = fopen(Args[Idx], "r");
+        char *Path = Args[Idx];
+        struct document *Doc = MakeDocument(Path);
 
-        if (!File) {
-            LogError("fopen");
-        }
-        else {
-            char Line[1024];
-            enum line_type LineType;
-
-            while ((LineType = ReadLine(File, Line, sizeof Line))) {
-                char *Prefix = "UNK";
-                switch (LineType) {
-                case LINE_NONE:    Prefix = "000"; break;
-                case LINE_EMPTY:   Prefix = "NUL"; break;
-                case LINE_ROW:     Prefix = "ROW"; break;
-                case LINE_COMMAND: Prefix = "COM"; break;
-                case LINE_COMMENT: Prefix = "REM"; break;
-                }
-
-                printf("%s.", Prefix);
-                switch (LineType) {
-                case LINE_ROW: {
-                    char Buf[512];
-                    enum token_type Type;
-                    struct row_lexer Lexer = { Line };
-
-                    while ((Type = NextRowToken(&Lexer, Buf, sizeof Buf))) {
-                        switch (Type) {
-                        case TOKEN_STATIC_CELL: printf("[%s]", Buf); break;
-                        case TOKEN_EXPR_CELL: printf("{%s}", Buf); break;
-                        InvalidDefaultCase;
-                        }
-                    }
-                } break;
-
-                case LINE_COMMAND: {
-                    char Buf[512];
-                    enum token_type Type;
-                    struct cmd_lexer Lexer = { Line };
-
-                    while ((Type = NextCmdToken(&Lexer, Buf, sizeof Buf))) {
-                        switch (Type) {
-                        case TOKEN_IDENT:
-                            printf("(%s)", Buf);
-                            break;
-                        InvalidDefaultCase;
-                        }
-                    }
-                } break;
-
-                case LINE_COMMENT: printf("%s", Line); break;
-                default: break;
-                }
-                printf("\n");
-            }
-
-            fclose(File);
-        }
+        printf("\n%s:\n", Path);
+        printf("%dx%d ", Doc->NumCol, Doc->NumRow);
+        printf("(%dx%d)\n", Doc->MaxCol, Doc->MaxRow);
+        PrintDocument(Doc);
+        DeleteDocument(Doc);
     }
 
     return 0;
--- src/main.h
+++ src/main.h
@@ -83,4 +83,7 @@ static_assert(sizeof (smm) == sizeof (dptr));
 #define STDOUT_FILENO 1
 #define STDERR_FILENO 2
 
+#define MAX(A,B) ((A) > (B)? (A): (B))
+#define MIN(A,B) ((A) < (B)? (A): (B))
+
 #endif
